6ea1c115e291eb101b58e2aeffa3fb11d6a62a50
woo628 <duddn2500@gmail.com>
2026-04-22 15:05:19 +0900

    Board 설정

---
+++ src/main/java/com/green/board/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.green.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/test")
+	@ResponseBody
+	public String test() {
+		return "<h2>hello world!</h2>";
+	}
+}
