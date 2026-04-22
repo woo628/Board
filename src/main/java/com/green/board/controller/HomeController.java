b9ebcab65d19c3120e261e56db62e3c6b342c88c
woo628 <duddn2500@gmail.com>
2026-04-22 15:40:29 +0900

    Board 설정

--- src/main/java/com/green/board/controller/HomeController.java
+++ src/main/java/com/green/board/controller/HomeController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/")  // jsp 파일을 찾겠다 http://localhost:8080
 	public String home() {
-		return "home";
+		return "home"; // home.jsp 
 	}
 	
-	@RequestMapping("/test")
-	@ResponseBody
+	@RequestMapping("/test") // jsp가 아니라 불러온다는 뜻
+	@ResponseBody // 서버가 data(html)을 내려보낸다
 	public String test() {
 		return "<h2>hello world!</h2>";
 	}
